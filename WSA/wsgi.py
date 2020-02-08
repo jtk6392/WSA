f1e29ab4f58288d07de04f064308d500960ad9d1
Jordan <jtk6392@g.rit.edu>
2020-02-08 12:21:32 -0500

    fixing original commit

--- WSA/wsgi.py
+++ WSA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BrickHack6 project.
+WSGI config for WSA project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrickHack6.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WSA.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BrickHack6.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WSA.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
