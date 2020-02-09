74f140eee102908cf189ce216b916243437561dc
nsp5488 <nsp5488@g.rit.edu>
2020-02-09 06:00:52 -0500

    I heard you like documentation

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.index),
     path('app/', views.application),
     path('app/store/', apps.get_store),
-    path('app/product/', apps.get_item_from_name),
     path('app/price/', apps.get_price_location_image),
-    path('app/products/',apps.get_products_list)
+    path('app/products/', apps.get_products_list)
 ]
